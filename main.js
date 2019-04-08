849af9a17de54a329ef4fe6821d063825a4550ab
alexsobiek <alex@alexsobiek.com>
2019-04-07 23:33:39 -0500

    Module Support

--- main.js
+++ main.js
@@ -1,12 +1,72 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var bodyParser = require("body-parser");
+var jwt        = require("jsonwebtoken");
+var path = require("path");
+var glob = require('glob');
 
-app.get('*', function (req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send("Advise Daemon Running");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+  next();
 });
 
+// Load Modules
+var getModules = function (callback) {
+  glob('modules/*', callback);
+};
+getModules(function (err, modules) {
+  if (err) {
+    console.log('Error', err);
+  } else {
+    modules.forEach(mod => {
+      var name = mod.substring(8)
+      if (require("./"+mod+"/config.json").enabled === "true") {
+        app.use("/module/"+name, require("./"+mod));
+        console.log("[Module] "+name + " loaded.");
+      }
+    });
+  }
+});
+
+function verifyToken(req, res, next){
+  console.log(req.query.token);
+  //Request header with authorization key
+  const bearerHeader = req.headers['authorization'];
+  //Check if there is  a header
+  if(typeof bearerHeader !== 'undefined'){
+      const bearer = bearerHeader.split(' ');
+      
+      //Get Token arrray by spliting
+      const bearerToken = bearer[1];
+      req.query.token = bearerToken;
+      //call next middleware
+      next();
+  }else{
+      console.log(1);
+      res.sendStatus(403);
+  }
+}
+
+//Test the Token
+app.get('/api/test', verifyToken, (req, res) => {
+  jwt.verify(req.query.token, 'SuperSecRetKey', (err, authData)=>{
+      if(err){
+          res.sendStatus(403);
+      }else{
+          res.json({
+              msg: "Success, you are signed in!",
+              authData
+          });
+      }
+  });
+});
+
+//Socket.io stuff for later
 io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
