40f8b2447e060b0d0c79e7501f2d30ab29effbe7
alexsobiek <alex@alexsobiek.com>
2019-04-15 16:07:28 -0500

    Config update

--- modules/local-auth/index.js
+++ modules/local-auth/index.js
@@ -2,6 +2,7 @@
 
 var path = require('path');
 const config = require( path.resolve( __dirname, "./config.json" ) );
+const global_config = require( path.resolve("./config.json" ) );
 var jwt        = require("jsonwebtoken");
 var express = require('express');
 var router = express.Router();
@@ -13,7 +14,7 @@ router.get('/login', (req, res) => {
         username: "johndoe",
         email: "john.doe@test.com"
     }
-    jwt.sign({user},'SuperSecRetKey', { expiresIn: 60 * 60 }, (err, token) => {
+    jwt.sign({user}, global_config.token_secret, global_config.token_duration, (err, token) => {
         res.json({token});
     });
 });
